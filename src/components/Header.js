a979baf6269ba41cce6eb59050a0b47404bbd800
VS <90790481+Vshalee@users.noreply.github.com>
2022-07-09 08:00:09 -0400

    first commit meme generator

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Header() {
+
+    return (<header>
+        <img src='../images/Troll Face.png' alt="meme-img" className="header--img" />
+        <h2 className="header--title">Meme Generator</h2>
+        <h5 className="react--title">React Course - Project 3</h5>
+    </header>
+    )
+
+
+
+
+}
---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Meme() {
+    return (<main>
+        <form className="form">
+            <input type="text" className="form--input"></input>
+            <input type="text" className="form--input"></input>
+            <button className="form--button">Get a new meme image 🖼</button>
+        </form>
+        <img src="" alt="meme img"></img>
+    </main>)
+
+
+
+}
